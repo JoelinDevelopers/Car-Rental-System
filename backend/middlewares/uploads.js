78488a954c8813bd42c71411043f0de3f5bc417d
Japheth Ochuku <japhethochuku@Japheths-MacBook-Pro.local>
2026-01-14 00:31:10 +0300

    Cloudinary config

--- backend/middlewares/uploads.js
+++ backend/middlewares/uploads.js
@@ -2,22 +2,14 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../cloudinary.js";
 
-// Cloudinary storage (NO local filesystem)
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "car-rentals",
     allowed_formats: ["jpg", "jpeg", "png"],
-    transformation: [{ width: 1000, height: 1000, crop: "limit" }],
   },
 });
 
-// Multer instance
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
-});
+const upload = multer({ storage });
 
 export default upload;
